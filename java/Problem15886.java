a12c5e2177df51e019d0fdb70f535f54a1397a0a
조대희 <joedaehui@ajou.ac.kr>
2025-01-06 10:13:38 +0900

    Solve 15886

---
+++ java/Problem15886.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Problem15886 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        String moving = br.readLine();
+        int present = 0;
+
+        for (int i = 0; i < n-1; i++) {
+            if (moving.charAt(i) == 'E' && moving.charAt(i + 1) == 'W') {
+                present++;
+            }
+        }
+
+        bw.write(String.valueOf(present));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
