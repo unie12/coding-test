1e30a8682f08da4e6546bee2f3875c16332d96f6
조대희 <joedaehui@ajou.ac.kr>
2025-01-07 11:19:48 +0900

    Solve 2606

---
+++ java/Problem2606.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Problem2606 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer>[] network = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            network[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            network[a].add(b);
+            network[b].add(a);
+        }
+
+        boolean[] visited = new boolean[n + 1];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(1);
+        visited[1] = true;
+        int count = 0;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            for (int next : network[current]) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    queue.offer(next);
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
+
+
