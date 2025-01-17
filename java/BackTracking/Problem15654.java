ef5ab613d810fb2143691cdc470d6899144b9b39
unie12 <joedaehui1@naver.com>
2025-01-17 12:48:32 +0900

    Solve 15654

---
+++ java/BackTracking/Problem15654.java
@@ -0,0 +1,55 @@
+package BackTracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Problem15654 {
+    static int n, m;
+    static boolean[] visited;
+    static int[] arr;
+    static int[] result;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        result = new int[m];
+        visited = new boolean[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int depth) {
+        if (depth == m) {
+            for (int val : result) {
+                sb.append(val).append(' ');
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                result[depth] = arr[i];
+                dfs(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
