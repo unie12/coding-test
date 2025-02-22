b9a8dc03f506f8cc2aede8d9f79f50ec8cb757bb
unie12 <joedaehui1@naver.com>
2025-02-22 15:40:11 +0900

    Add solution for Problem 11404: Implement Floyd-Warshall algorithm for finding shortest paths in a graph

---
+++ java/graph/Problem11404.java
@@ -0,0 +1,54 @@
+package graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem11404 {
+    static int n, m;
+    static int[][] dist;
+    static final int INF = 987654321;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+
+        dist = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) dist[i][j] = 0;
+                else dist[i][j] = INF;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            dist[a][b] = Math.min(dist[a][b], c);
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                sb.append(dist[i][j] == INF ? 0 : dist[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+
+    }
+}
